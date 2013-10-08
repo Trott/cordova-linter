a88032584beb3ee61c670c5023716092a935a822
Rich Trott <rtrott@gmail.com>
2013-10-07 21:00:43 -0700

    bare bones command-line-interface

---
+++ spec/cli.spec.js
@@ -0,0 +1,29 @@
+/* global describe:false */
+/* global beforeEach:false */
+/* global spyOn:false */
+/* global it:false */
+/* global expect:false */
+var shell = require('shelljs'),
+    path = require('path'),
+    bin = 'node ' + path.resolve(path.join(__dirname, '..', 'bin', 'cordova-linter'));
+
+describe('$ cordova-linter [options] commands', function() {
+    beforeEach(function() {
+        spyOn(process.stdout, 'write');
+    });
+
+    // it('should support no arguments', function() {
+    //     var process = shell.exec(bin + '', { silent: true });
+    //     expect(process.output).toMatch('Usage:');
+    // });
+
+    it('should support commands', function() {
+        var process = shell.exec(bin + ' version', { silent: true });
+        expect(process.output).toMatch(/^\w+\.\w+\.\w+/);
+    });
+
+    it('should support options', function() {
+        var process = shell.exec(bin + ' --version', { silent: true });
+        expect(process.output).toMatch(/^\w+\.\w+\.\w+/);
+    });
+});
