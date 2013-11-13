56c4312db74ba3b10d17ad54e93f6483c1a7fc6b
Rich Trott <rtrott@gmail.com>
2013-11-12 23:29:41 -0800

    pass path argument

--- spec/cli.spec.js
+++ spec/cli.spec.js
@@ -7,19 +7,22 @@ var shell = require('shelljs'),
     path = require('path'),
     bin = 'node ' + path.resolve(path.join(__dirname, '..', 'bin', 'cordova-linter'));
 
-describe('$ cordova-linter [options] commands', function() {
+describe('$ cordova-linter [options] [projectPath]', function() {
     beforeEach(function() {
         spyOn(process.stdout, 'write');
     });
 
     it('should support no arguments', function() {
-        var process = shell.exec(bin + '', { silent: true });
-        expect(process.output).toMatch(/lint free/);
+        expect ( function () {
+            var myProcess = shell.exec(bin, { silent: true });
+            expect(myProcess.output.indexOf('Linting ' + process.cwd())).toEqual(0);
+        });
     });
 
-    it('should support commands', function() {
-        var process = shell.exec(bin + ' version', { silent: true });
-        expect(process.output).toMatch(/^\w+\.\w+\.\w+/);
+    it('should support a path argument', function() {
+        var projectPath = path.join('fixture', 'project', 'Baz');
+        var process = shell.exec(bin + ' ' + projectPath, { silent: true });
+        expect(process.output.indexOf('Linting ' + projectPath)).toEqual(0);
     });
 
     it('should support options', function() {
@@ -29,6 +32,6 @@ describe('$ cordova-linter [options] commands', function() {
 
     it ('should support unknown commands', function() {
         var process = shell.exec(bin + ' deep_sea_fangly_fish', { silent: true });
-        expect(process.output).toMatch('Usage:');
+        expect(process.output).toMatch('Linting deep_sea_fangly_fish');
     });
 });
