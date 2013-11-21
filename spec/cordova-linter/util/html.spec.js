7bf52fb9fc0117d2c6ea11b63eaade0ec90d9d1c
Rich Trott <rtrott@gmail.com>
2013-11-20 17:22:04 -0800

    add spec

--- spec/cordova-linter/util/html.spec.js
+++ spec/cordova-linter/util/html.spec.js
@@ -21,9 +21,15 @@ describe('html', function() {
             });
         });
 
-        describe('when src attribute present', function () {
+        describe('when src attribute present and no type is set', function () {
             it('should return src', function () {
                 expect(html.findJs('<script src="foo.js"></script>')[0].src).toEqual('foo.js');
+            });
+        });
+
+        describe('when src attribute present and type is set', function () {
+            it('should return src', function () {
+                expect(html.findJs('<script type="text/javascript" src="cordova.js"></script>')[0].src).toEqual('cordova.js');
             });
         });
     });
